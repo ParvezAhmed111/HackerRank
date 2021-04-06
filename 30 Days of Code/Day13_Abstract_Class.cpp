f314ed50afc28ee5928e01a2405a9776d3d0a7f5
Sheikh Parvez Ahmed <46757345+ParvezAhmed111@users.noreply.github.com>
2021-04-06 23:58:10 +0530

    Create Day13_Abstract_Class.cpp

---
+++ 30 Days of Code/Day13_Abstract_Class.cpp
@@ -0,0 +1,19 @@
+
+// Write your MyBook class here
+
+    //   Class Constructor
+    //   
+    //   Parameters:
+    //   title - The book's title.
+    //   author - The book's author.
+    //   price - The book's price.
+    //
+    // Write your constructor here
+    
+    
+    //   Function Name: display
+    //   Print the title, author, and price in the specified format.
+    //
+    // Write your method here
+    
+// End class
