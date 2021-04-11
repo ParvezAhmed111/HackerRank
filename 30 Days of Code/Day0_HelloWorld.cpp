30b85b8b624b06a89ebbb53ae6106b4042427bc2
Sheikh Parvez Ahmed <46757345+ParvezAhmed111@users.noreply.github.com>
2021-04-11 22:15:19 +0530

    Update Day0_HelloWorld.cpp

--- 30 Days of Code/Day0_HelloWorld.cpp
+++ 30 Days of Code/Day0_HelloWorld.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 using namespace std;
 int main() {
-    // Declare a variable named 'input_string' to hold our input.
+    // Declare a variable named 'input_string' to hold our input
     string input_string; 
     
     // Read a full line of input from stdin (cin) and save it to our variable, input_string.
